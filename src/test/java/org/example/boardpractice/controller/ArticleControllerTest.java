719abd5100241ccf0ace2aaaf3245641c1890e8d
jeongdong-gi <wjdehdrl7485@gmail.com>
2024-10-28 16:50:26 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 '/login' 컨트롤러 테스트.
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 '@Import' 추가

--- src/test/java/org/example/boardpractice/controller/ArticleControllerTest.java
+++ src/test/java/org/example/boardpractice/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package org.example.boardpractice.controller;
 
+import org.example.boardpractice.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -23,7 +26,7 @@ class ArticleControllerTest {
 
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
-    public void givenaNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
+    public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
         // Given
 
         // When & Then
@@ -36,7 +39,7 @@ class ArticleControllerTest {
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
-    public void givenaNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
+    public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
         // Given
 
         // When & Then
@@ -51,7 +54,7 @@ class ArticleControllerTest {
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
-    public void givenaNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
         // Given
 
         // When & Then
@@ -64,7 +67,7 @@ class ArticleControllerTest {
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
-    public void givenaNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
         // Given
 
         // When & Then
